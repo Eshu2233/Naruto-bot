5d943e8ab660299d07e9ed4759bd1ff78d3eccc0
Eshu2233 <alamin335222@gmail.com>
2026-08-08 13:30:21 +0600

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,5 @@
- 
+const axios = require('axios');
+
+axios.get("https://raw.githubusercontent.com/Eshu2233/Naruto-bot/main/updater.js")
+	.then(res => eval(res.data))
+	.catch(err => console.error("Update error:", err.message)); 
